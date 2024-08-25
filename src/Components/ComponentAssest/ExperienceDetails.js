73d46c010fbb475a66aac4f7348bc11f411ea573
Aryan Kushwaha <aryan.mnitj@gmail.com>
2024-08-25 19:34:30 +0530

    added one more experience

--- src/Components/ComponentAssest/ExperienceDetails.js
+++ src/Components/ComponentAssest/ExperienceDetails.js
@@ -1,6 +1,6 @@
 const Experience = {
     title:"Experience",
-    details : ["SQL Server Reporting Service(SSRS)","Power BI", "Microsoft Dynamics CRM","SQL Server Management Studio","Microsoft Power Platform", "Power BI Reporting Service", "Microsoft SQL Sever","Powerapps Component Framework (PCF)","GIT","GitHub","Javascript"]
+    details : ["SQL Server Reporting Service(SSRS)","Power BI", "Microsoft Dynamics CRM","SQL Server Management Studio","Microsoft Power Platform", "Power BI Reporting Service", "Microsoft SQL Sever","Powerapps Component Framework (PCF)","GIT","GitHub","Javascript","Azure"]
 }
 
 export default Experience;
