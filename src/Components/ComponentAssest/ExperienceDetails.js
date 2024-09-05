d2799efd4407a6083bee7661e06288cabfbbed94
Aryan Kushwaha <arykushwaha@deloitte.com>
2024-09-05 19:59:43 +0530

    added some experiences

--- src/Components/ComponentAssest/ExperienceDetails.js
+++ src/Components/ComponentAssest/ExperienceDetails.js
@@ -1,6 +1,6 @@
 const Experience = {
     title:"Experience",
-    details : ["SQL Server Reporting Service(SSRS)","Power BI", "Microsoft Dynamics CRM","SQL Server Management Studio","Microsoft Power Platform", "Power BI Reporting Service", "Microsoft SQL Sever","Powerapps Component Framework (PCF)","GIT","Javascript","Azure"]
+    details : ["SQL Server Reporting Service(SSRS)","Power BI", "Microsoft Dynamics CRM","SQL Server Management Studio","Microsoft Power Platform", "Power BI Reporting Service", "Microsoft SQL Sever","Powerapps Component Framework (PCF)","GIT","Javascript","Azure",". Net"]
 }
 
 export default Experience;
